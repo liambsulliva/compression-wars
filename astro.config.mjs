23d71bd0b0a385f6f252a4dd4a1cd4275f75c7a8
Liam <75332524+liambsulliva@users.noreply.github.com>
2024-11-01 16:26:41 -0400

    add jxl image w/ vite trickery

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from "astro/config";
 import deno from "@astrojs/deno";
 import tailwind from "@astrojs/tailwind";
+import { imagetools } from "vite-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   adapter: deno(),
+  vite: {
+    plugins: [imagetools()],
+  },
 });
---
+++ src/image.d.ts
@@ -0,0 +1,4 @@
+declare module '*.jxl?url&skip' {
+  const value: string;
+  export default value;
+}
